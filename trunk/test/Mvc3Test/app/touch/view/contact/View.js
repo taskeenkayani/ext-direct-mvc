a5f790ebe1a9bce10ef77f6880bd4dad1aa5dc50
elishnevsky <elishnevsky@59ee3dda-d968-11de-b113-f7137c4feea6>
2012-03-04 14:59:09 +0000

    working on the test app...

--- trunk/test/Mvc3Test/app/touch/view/contact/View.js
+++ trunk/test/Mvc3Test/app/touch/view/contact/View.js
@@ -1,5 +1,5 @@
 ﻿Ext.define('Test.view.contact.View', {
-    extend: 'Ext.Component',
+    extend: 'Ext.Container',
     xtype: 'contact-view',
     
     config: {
@@ -10,6 +10,14 @@
             '<h3>{FirstName} {LastName}</h3>',
             '<div><span class="label">Email: </span><a href="mailto:{Email}">{Email}</a></div>',
             '<div><span class="label">Birth Date: </span>{BirthDate:date("F j, Y")}</div>'
-        ].join('')
+        ].join(''),
+        items: [{
+            xtype: 'component',
+            top: 15,
+            right: 15,
+            width: 30,
+            height: 30,
+            style: 'background-color:red;'
+        }]
     }
 });
